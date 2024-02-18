7530823cfdbdb86e32dd458aa57ad25cad36a441
Liam Avella-Pisera <50953884+paperclipmaximizer@users.noreply.github.com>
2024-02-18 18:17:59 +1100

    add base gameplay (#1)
    * game class and player constructor
    
    * readme add
    
    * cleanup file
    
    * add upperbounds checking
    
    * remove readme course reference

---
+++ Player.java
@@ -0,0 +1,34 @@
+public class Player {
+    /**
+     * Player(String name)
+     * initializes a player with an encapsulated name field
+     * exposes hidden field with setter and getter methods
+     */
+    private String name;
+    public Player(String _name){
+        this.name = _name;
+    }
+//    private Integer score; // can't remember if this is required
+    public String getName(){ 
+        return name;
+    }
+    public void setName(String newName) {
+	    this.name = newName;
+    }
+    /**
+     * This method returns the number of stones the player wants to remove in their turn
+     * use stones to check if the move is legal
+     */
+    public Integer removeStone(int upperBounds){
+        System.out.println(name + "'s turn - remove how many?");
+        int stonesToRemove = Nim.input.nextInt();
+        if (stonesToRemove > upperBounds){
+            stonesToRemove = upperBounds;
+            System.out.println("You must remove at most respect the upperbounds.\nDefaulting to " + upperBounds + " stones removed");
+        }else if (stonesToRemove < 1){
+            stonesToRemove = 1;
+            System.out.println("You must remove at least one stone\nDefaulting to 1 stones removed");
+        }
+        return (stonesToRemove);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # game-of-nim
-game of nim for COMP90041
+## Requires
+ - A Java runtime environment
+ - a JDK to build the project
+## Build
+ - The project can be built by running the following in the root of the project
+```sh
+javac Nim.java Player.java
+```
+## Run
+ - The project can be run by the following command in the root of the project
+```sh
+java Nim
+```
